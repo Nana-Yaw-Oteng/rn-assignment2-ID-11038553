ad790d0b96d3e74d00825cdd0d8d13b0bd25531c
Nana-Yaw-Oteng <nyooteng@st.ug.edu.gh>
2024-05-24 16:08:56 +0000

    Boldened name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:24}}>My name is Oteng Nana Yaw Owusu </Text>
+      <Text style={{fontSize:24}}>My name is <Text style={{fontWeight:'bold'}}> Oteng Nana Yaw Owusu </Text> </Text>
       <StatusBar style="auto" />
     </View>
   );
